0a566ea7dd386433a60af1ae7f78572e3b404d98
Nadavrav <nadavrav@post.bgu.ac.il>
2025-05-20 15:25:08 +0300

    added some texts to home and about

--- solvinery-overview/src/App.js
+++ solvinery-overview/src/App.js
@@ -38,13 +38,13 @@ function App() {
       <div className="content">
         {/* Home Section */}
         <div id="home" className="home">
-          <h1>Welcome to the Home Page</h1>
-          <p>This is the home section of your page.</p>
+          <h1>Solvinery</h1>
+          <p>Solving complex problems like shift scheduling or task managment can be overwhelming, especially without technical expertise. Our system enables anyone to easily access powerful solutions. Programmers write and share these solutions using advanced tools,, and with our system, you can apply them instantly for your own needs.</p>
         </div>
 
         {/* About Section */}
         <div id="about" className="about">
-          <h1>About Us</h1>
+          <h1>About The Project</h1>
           <p>This is the about section of your page. Here, you can describe more about the project or website.</p>
         </div>
       </div>
